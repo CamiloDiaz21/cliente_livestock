ae83fae99cd19aa06ca830fe2bfd1ebb3883f114
kamilodiaz0521@gmail.com <kamilodiaz0521@gmail.com>
2025-04-02 22:38:07 -0500

    creacion de cliente login (ingresar cuenta)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'cliente_livestock' title`, () => {
+  it(`should have the 'cliente_prueba' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cliente_livestock');
+    expect(app.title).toEqual('cliente_prueba');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cliente_livestock');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cliente_prueba');
   });
 });
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButton,
+    FormsModule,
+    FormsModule,
+    MatCheckboxModule,
+
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  correo: string = '';
+    constructor(private router: Router) {}
+    hidePassword=true;
+
+    goToRegister(){
+      console.log('Boton de registro clickeado');
+    }
+    login(){
+      alert('iniciando sesion...');
+    }
+    recoverPassword(){
+      alert('recuperando contraseña...');
+    }
+    createAccount(){
+      alert('creando cuenta...');
+    }
+}
