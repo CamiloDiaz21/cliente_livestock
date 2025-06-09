05533f9f469118a78f867aa1fc5d64a5f70db33b
kamilodiaz0521@gmail.com <kamilodiaz0521@gmail.com>
2025-06-09 15:06:55 -0500

    ajustes post hoja de vida

--- src/app/pages/hoja-vida/hoja-vida.component.ts
+++ src/app/pages/hoja-vida/hoja-vida.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-hoja-vida',
+  standalone: true,
   imports: [
-
+    CommonModule,
+    MatCardModule,
+    MatButtonModule
   ],
   templateUrl: './hoja-vida.component.html',
-  styleUrl: './hoja-vida.component.css'
+  styleUrls: ['./hoja-vida.component.css']
 })
 export class HojaVidaComponent {
+  selectedFile: File | null = null;
+  fileName: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      this.fileName = this.selectedFile.name;
+    }
+  }
+
+  onSubmit() {
+    if (!this.selectedFile) return;
+
+    const formData = new FormData();
+    formData.append('cv', this.selectedFile);
+
+    this.http.post('https:/localhost:8088/v1/hoja-vida', formData).subscribe({
+      next: res => console.log('CV subido con éxito'),
+      error: err => console.error('Error al subir CV', err)
+    });
+  }
 }
